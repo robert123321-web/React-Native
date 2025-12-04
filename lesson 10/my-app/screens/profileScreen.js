e46a9a5304d342611c321c9077f527c505f38c66
robert123321-web <roberthaxhiu2012@gmail.com>
2025-12-04 20:43:32 +0100

    lesson 10

---
+++ lesson 10/my-app/screens/profileScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, StyleSheet, View } from "react-native";
+
+ function ProfileScreen() {
+  return (
+    <View>
+      <Text >This is the Profile Screen</Text>
+    </View>
+  );
+}
+ export default ProfileScreen;
